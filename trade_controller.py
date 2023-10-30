f7c5237a60daea359abca2f28f97fb2c3b9e48d4
Wafic Chahine <wafic@Wafics-MacBook-Pro.local>
2023-10-30 13:49:24 -0500

    class completed

--- trade_controller.py
+++ trade_controller.py
@@ -8,21 +8,19 @@ import time
 from dotenv import load_dotenv, find_dotenv
 
 
-class TradeController():
-
+class TradeController:
     load_dotenv(find_dotenv())
     api_key = os.environ.get("API_KEY")
     api_sec = os.environ.get("API_SECRET")
 
     def __init__(self, api_key=api_key) -> None:
         self.headers = {
-        'API-Key': api_key
+            'API-Key': api_key
         }
 
-
-    def generate_nonce(self):
+    @staticmethod
+    def generate_nonce():
         return int(time.time())
-
 
     def get_kraken_signature(self, uripath, data, secret=api_sec):
         postdata = urllib.parse.urlencode(data)
@@ -33,28 +31,82 @@ class TradeController():
         sigdigest = base64.b64encode(mac.digest())
         self.headers['API-Sign'] = sigdigest.decode()
 
+    @staticmethod
+    def get_server_time(url="https://api.kraken.com/0/public/Time"):
+        return requests.get(url).json()
 
-    def get_asset_info(self, asset_name, url="https://api.kraken.com/0/public/Assets"):
-        post_data = {
-            'asset': asset_name
+    @staticmethod
+    def get_system_status(url="https://api.kraken.com/0/public/SystemStatus"):
+        return requests.get(url).json()
+
+    @staticmethod
+    def get_asset_info(assets, url="https://api.kraken.com/0/public/Assets"):
+        assets_str = ",".join(assets) if isinstance(assets, list) else assets
+        params = {
+            'asset': assets_str
         }
+        return requests.get(url=url, params=params).json()
 
-        return requests.post(url=url, data=post_data).json()
-    
-    
-    def get_deposit_methods(self, asset_name, url="https://api.kraken.com/0/private/DepositMethods", uri='/0/private/DepositMethods'):
-        post_data = {
+    @staticmethod
+    def get_tradable_asset_pairs(pairs, url="https://api.kraken.com/0/public/AssetPairs"):
+        pairs_str = ",".join(pairs) if isinstance(pairs, list) else pairs
+        params = {
+            'pair': pairs_str
+        }
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_ticker_info(pairs, url="https://api.kraken.com/0/public/Ticker"):
+        pairs_str = ",".join(pairs) if isinstance(pairs, list) else pairs
+        params = {
+            'pair': pairs_str
+        }
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_ohlc_data(pair, since, interval=1, url="https://api.kraken.com/0/public/OHLC"):
+        params = {
+            'pair': pair,
+            'interval': interval,
+            'since': since
+        }
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_order_book(pair, count, url="https://api.kraken.com/0/public/Depth"):
+        params = {
+            "pair": pair,
+            "count": count
+        }
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_recent_trades(pair, since, count=1000, url="https://api.kraken.com/0/public/Trades"):
+        params = {
+            "pair": pair,
+            "since": since,
+            "count": count
+        }
+        return requests.get(url, params=params).json()
+
+    @staticmethod
+    def get_recent_spreads(pair, since, url="https://api.kraken.com/0/public/Spread"):
+        params = {
+            "pair": pair,
+            "since": since
+        }
+        return requests.get(url, params=params).json()
+
+    def get_deposit_methods(self, asset_name, url="https://api.kraken.com/0/private/DepositMethods",
+                            uri='/0/private/DepositMethods'):
+        params = {
             'nonce': self.generate_nonce(),
             'asset': asset_name
         }
-        print(post_data)
-        self.get_kraken_signature(uri, data=post_data)
-
-        return requests.post(url, headers=self.headers, data=post_data).json()
-        
+        self.get_kraken_signature(uri, data=params)
+        return requests.get(url, headers=self.headers, params=params).json()
 
 
 if __name__ == '__main__':
     trade = TradeController()
-    btc_info = trade.get_deposit_methods('BTC')
-    print(btc_info)
+    print(trade.get_recent_spreads("BTC/USD", since=1616663618))
